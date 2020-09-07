dc9d3c047b30123804f30586255c9bb589206a2c
Nikolay Valkov <nikolay.valkov@student.manchester.ac.uk>
2020-09-07 16:53:37 +0300

    Add json api for generated images
    Three new endpoints have been added for each of the GANs. There
    endpoints return images in a Json format. The Json has a completion
    status that states if the image was delived and an image attribute
    which contains an image converted to BytesIO then encoded to Base64
    then endoded as UTF-8 to be a string.

--- src/generateme/app.py
+++ src/generateme/app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, send_file, Response, url_for
+from flask import Flask, render_template, send_file, Response, url_for, jsonify
 from PIL import Image
 import numpy as np
 import io
 import os
+import pybase64
 from generateme.LinearGan import getLinearImage
 from generateme.DCGan import getConvImage
 from generateme.StyleGan import getStyleImage
@@ -30,6 +31,18 @@ def showImageConv():
 @app.route("/getLinearImage")
 def showImageLinear():
     return getImage(getLinearImage())
+    
+@app.route("/api/getStyleImage")
+def showImageStyleApi():
+    return getJson(getStyleImage())
+    
+@app.route("/api/getConvImage")
+def showImageConvApi():
+    return getJson(getConvImage())
+    
+@app.route("/api/getLinearImage")
+def showImageLinearApi():
+    return getJson(getLinearImage())
 
 def getImage(img):
     file_object = io.BytesIO()
@@ -40,6 +53,13 @@ def getImage(img):
     response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0"
     return response
     
+def getJson(img):
+    file_object = io.BytesIO()
+    img.save(file_object, "PNG")  
+    file_object.seek(0)
+    base64image = (pybase64.b64encode(file_object.getvalue(), altchars="_:")).decode("utf-8")
+    response =  { "Completion_Status": "Success", "Image": base64image}
+    return jsonify(response) # send the result to client
     
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", port = 5000)
